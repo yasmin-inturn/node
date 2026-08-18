2679a8fb5f39b0c7ee0f7b27835780cb03f898f4
yasmini-mp3 <inturnyasmin@gmail.com>
2026-08-18 19:23:14 -0300

    add "Buffers" study

--- streams/buffer.js
+++ streams/buffer.js
@@ -1,3 +1,13 @@
 // buffers is the part of the memory that is used to store data temporarily while it is being moved from one place to another
 // In this case, it is used to store the chunks of data that are being sent to the server. 
 
+// buffer keeps the data in binary format, which is more efficient than storing it as a string.
+// it was created cuz of the incapability of the JavaScript to deal with binary data
+
+// example:
+
+const buf = Buffer.from('Hello, World!')
+
+console.log(buf) // <Buffer 48 65 6c 6c 6f 2c 20 57 6f 72 6c 64 21> -> Hello, World! (in binary format)
+
+// this is way more efficient than storing it as a string, which would take up more memory and be slower to process.
